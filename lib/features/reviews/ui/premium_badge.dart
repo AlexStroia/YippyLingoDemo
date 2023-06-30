e42fb09b2a1781b409234d8e9cd7d0a8374b0721
alexandru.stroia <alexandru.stroia>
2023-06-30 13:20:29 -0400

    Fixes & add tests

--- lib/features/reviews/ui/premium_badge.dart
+++ lib/features/reviews/ui/premium_badge.dart
@@ -9,15 +9,17 @@ class PremiumBadge extends StatelessWidget {
     final theme = Theme.of(context).extension<AppTheme>()!;
     final fonts = theme.fonts;
     return Container(
-      width: 123,
       decoration: BoxDecoration(
         color: const Color(0xFF38C77D), // Background color
         borderRadius: BorderRadius.circular(8), // Border radius
       ),
       padding: const EdgeInsets.all(8), // Padding
       child: Row(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Container(
+          Flexible(child:Container(
             decoration: BoxDecoration(
               shape: BoxShape.circle,
               border: Border.all(color: Colors.white, width: 1), // White border
@@ -28,13 +30,12 @@ class PremiumBadge extends StatelessWidget {
               color: Colors.white,
               size: 16,
             ),
-          ),
+          )),
           const SizedBox(width: 8), // Space between the check icon and text
-          Expanded(
-              child: Text(
+           Flexible(child:Text(
             'Premium',
             style: fonts.p2.copyWith(color: Colors.white), // Text style
-          )),
+          ))
         ],
       ),
     );
